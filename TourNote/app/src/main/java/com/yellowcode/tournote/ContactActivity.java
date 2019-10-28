69d51b4fec82b6eb388d7840a9d36970163cd098
Yellow Code <yellowcode.books@gmail.com>
2019-10-28 15:26:55 +0700

    Upload source code for Bai 26 - Activity (updated on 2019)

---
+++ TourNote/app/src/main/java/com/yellowcode/tournote/ContactActivity.java
@@ -0,0 +1,28 @@
+package com.yellowcode.tournote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
